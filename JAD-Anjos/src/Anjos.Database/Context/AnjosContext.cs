17b3ba68142b2511f7e59ff4dae77033a04bbe5d
Jamily Magalhães <jamily.silva@basisti.com.br>
2024-06-18 13:57:23 -0400

    Feat: Context

---
+++ JAD-Anjos/src/Anjos.Database/Context/AnjosContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Anjos.Database.Context;
+
+public class AnjosContext : DbContext
+{
+    public AnjosContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        AddMappingsDynamically(builder);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="builder"></param>
+    private static void AddMappingsDynamically(ModelBuilder builder)
+    {
+        var currentAssembly = typeof(AnjosContext).Assembly;
+        var efMappingTypes = currentAssembly.GetTypes().Where(t =>
+            t.FullName != null && t.FullName.StartsWith("Anjos.Database.Mapping.") && t.FullName.EndsWith("Mapping"));
+
+        foreach (var map in efMappingTypes.Select(Activator.CreateInstance))
+        {
+            builder.ApplyConfiguration((dynamic)map!);
+        }
+    }
+}
