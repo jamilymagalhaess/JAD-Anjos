d691226cf10234daa05e67ec6a905646f7f327c8
Jamily Magalhães <jamily.silva@basisti.com.br>
2024-06-19 14:25:36 -0400

    Feat: Criado os dbsets

--- JAD-Anjos/src/Anjos.Database/Context/AnjosContext.cs
+++ JAD-Anjos/src/Anjos.Database/Context/AnjosContext.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Anjos.Model.Entities.Entrada;
+using Anjos.Model.Entities.Parcela;
+using Anjos.Model.Entities.Produto;
+using Anjos.Model.Entities.Venda;
+using Anjos.Model.Entities.Conta;
+using Anjos.Model.Entities.Categoria;
 
 
 namespace Anjos.Database.Context;
 
 public class AnjosContext : DbContext
 {
-    public AnjosContext(DbContextOptions options) : base(options) { }
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produto { get; set; }
+    public DbSet<Parcela> Parcela { get; set; }
+    public DbSet<Entrada> Entrada { get; set; }
+    public DbSet<EntradaProduto> EntradaProduto { get; set; }
+    public DbSet<ContaPagar> ContaPagar { get; set; }
+    public DbSet<ContaReceber> ContaReceber { get; set; }
+    public DbSet<Venda> Venda { get; set; }
+    public DbSet<VendaProduto>VendaProduto { get; set; }
+
+    public AnjosContext(DbContextOptions<AnjosContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
