9ea116e254b1cc83a7bf2a88b351c0ae338dc002
Jamily Magalhães <jamily.silva@basisti.com.br>
2024-06-25 16:14:42 -0400

    Feat: Um Repositorio aqui generico

---
+++ JAD-Anjos/src/Anjos.Database/Repositories/IRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Anjos.Database.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task Adicionar(TEntity entidade);
+        Task Remover(TEntity entidade);
+    }
+}
---
+++ JAD-Anjos/src/Anjos.Database/Repositories/Repository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Anjos.Database.Context;
+using Anjos.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Anjos.Database.Repositories
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly AnjosContext _context;
+
+        protected Repository(AnjosContext context)
+        {
+            _context = context;
+        }
+
+        public virtual async Task Adicionar(TEntity entidade)
+        {
+            await _context.AddAsync(entidade);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task Remover(TEntity entidade)
+        {
+            _context.Remove(entidade);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
